6dd09420bfd8ced700c2ac9732ec91ed9d759522
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2022-04-10 21:32:09 +0700

    fix: create handler function as props for button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import PrimaryButton from "../components/buttons/primary-button";
+import SecondaryButton from "../components/buttons/secondary-button";
 
 const App = () => {
+  const onClickDecrement = () => console.log('decrement');
+  const onClickIncrement = () => console.log('increment');
+  const onClickReset = () => console.log('reset');
+
   return (
     <div className='main-page'>
       <h2>Counter</h2>
       <div className='main-content'>
-        <button>-</button>
+        <PrimaryButton label='-' handler={onClickDecrement} />
         <p className='counter-result'>0</p>
-        <button>+</button>
+        <PrimaryButton label='+' handler={onClickIncrement} />
       </div>
-      <button>Reset</button>
+      <SecondaryButton label='Reset' handler={onClickReset} />
     </div>
   );
 };
