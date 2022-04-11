e139756aabda7953c1e9d93d7019cd7e4a1c033e
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2022-04-11 11:47:44 +0700

    style: add white space before returned type IAction

--- src/services/actions/counter/index.tsx
+++ src/services/actions/counter/index.tsx
@@ -1,19 +1,19 @@
 import ACTIONS from "../../constants/actionTypes";
 import { IAction } from "../../models";
 
-export const decrement = ():IAction => (
+export const decrement = (): IAction => (
   {
     type: ACTIONS.DECREMENT
   }
 )
 
-export const increment = ():IAction => (
+export const increment = (): IAction => (
   {
     type: ACTIONS.INCREMENT
   }
 )
 
-export const reset = ():IAction => (
+export const reset = (): IAction => (
   {
     type: ACTIONS.RESET
   }
