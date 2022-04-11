9862169d5aba6f1b925238105ad339ce47e2b993
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2022-04-11 11:45:57 +0700

    fix: add returned type as interface action for all action methods

--- src/services/actions/counter/index.tsx
+++ src/services/actions/counter/index.tsx
@@ -1,18 +1,19 @@
 import ACTIONS from "../../constants/actionTypes";
+import { IAction } from "../../models";
 
-export const decrement = () => (
+export const decrement = ():IAction => (
   {
     type: ACTIONS.DECREMENT
   }
 )
 
-export const increment = () => (
+export const increment = ():IAction => (
   {
     type: ACTIONS.INCREMENT
   }
 )
 
-export const reset = () => (
+export const reset = ():IAction => (
   {
     type: ACTIONS.RESET
   }
