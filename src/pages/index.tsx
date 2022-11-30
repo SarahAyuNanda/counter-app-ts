68d34d157c4a5a4bed88eabeb5f2ae04c0ca244a
Sarah Ayu Nanda <nanda.sarahayu@gmail.com>
2022-11-30 14:06:05 +0700

    feat: replace 2 buttons with base button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import PrimaryButton from "../components/buttons/primary-button";
-import SecondaryButton from "../components/buttons/secondary-button";
+import BaseButton from "../components/buttons/BaseButton";
 import CONSTANTS from "../services/constants";
 import { IAppState } from "../services/models";
 import * as Action from "../services/actions/counter"
@@ -11,7 +10,7 @@ const App = () => {
   const counter = useSelector((state: IAppState) => state.counter);
 
   const disableReset = counter.number === 0;
-  
+
   const onClickDecrement = () => dispatch(Action.decrement());
   const onClickIncrement = () => dispatch(Action.increment());
   const onClickReset = () => dispatch(Action.reset());
@@ -20,11 +19,11 @@ const App = () => {
     <div className='main-page'>
       <h2>{CONSTANTS.TITLE.COUNTER}</h2>
       <div className='main-content'>
-        <PrimaryButton label={CONSTANTS.SYMBOL.MINUS} handler={onClickDecrement} />
+        <BaseButton label={CONSTANTS.SYMBOL.MINUS} onClick={onClickDecrement} variant={'primary'} />
         <p className='counter-result'>{counter.number}</p>
-        <PrimaryButton label={CONSTANTS.SYMBOL.PLUS} handler={onClickIncrement} />
+        <BaseButton label={CONSTANTS.SYMBOL.PLUS} onClick={onClickIncrement} variant={'primary'} />
       </div>
-      <SecondaryButton label={CONSTANTS.DESCRIPTION.RESET} handler={onClickReset} disabled={disableReset} />
+      <BaseButton label={CONSTANTS.DESCRIPTION.RESET} onClick={onClickReset} variant={'secondary'} disabled={disableReset} />
     </div>
   );
 };
